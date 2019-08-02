a65a3404d3b00daa800d291417bae391b6e71e4f
José Teles Gouveia <zepedrogouveia@gmail.com>
2019-08-02 16:21:52 +0100

    Update ga.php

--- ga.php
+++ ga.php
@@ -1,39 +1,21 @@
 <?php
 
-
 $GA = "XX-XXXXXXXX-X"; // insert your Google Analytics ID Here
 
-
-if (isset($_COOKIE['RGB-Color'])) {
-  $sessid = $_COOKIE['RGB-Color'];
+if (isset($_COOKIE['GA'])) {
+  $sessid = $_COOKIE['GA'];
 } else {
-	$cookie_name = 'RGB-Color';
-	$cookie_value = ''.rand(100,255).'-'.rand(100,255).'-'.rand(100,255).'';
+	$cookie_name = 'GA';
+	$cookie_value = mt_rand(100000,999999);
 	setcookie($cookie_name, $cookie_value, time() + (86400 * 365), "/"); // 86400 = 1 day
 	$sessid = $cookie_value;
 };
 
-
-if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-	$ip = $_SERVER['HTTP_CLIENT_IP'];
-} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-	$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-} else {
-	$ip = $_SERVER['REMOTE_ADDR'];
-};
-
-
+$uip = urlencode($_SERVER['REMOTE_ADDR']);
 $ua = urlencode($_SERVER['HTTP_USER_AGENT']);
-
-
-$dp = (basename($_SERVER['PHP_SELF']));
-
-
-if (!empty($_SERVER['HTTP_REFERER'])) {
-	$ref = $_SERVER['HTTP_REFERER'];
-} else {
-	$ref = '';
-};
+$dp = urlencode(basename($_SERVER['PHP_SELF']));
+$ref = urlencode($_SERVER['HTTP_REFERER']);
+$cid = md5($sessid.$ip.$ua);
 
 $url = 'https://www.google-analytics.com/collect?v=1&t=pageview&tid='.$GA.'&cid='.$sessid.'&uip='.$ip.'&ua='.$ua.'&dp='.$dp.'&dr='.$ref;
 file_get_contents($url);
